54af218dc7462236b0ebfc4d1c13163efc7fb777
Jose Álvarez <iphone4jose@icloud.com>
2021-06-25 17:15:50 +0200

    add Comment-list component

---
+++ src/app/components/route/comment-list/comment-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Comment } from "../../../models/domains/comment.domain";
+import Swal from "sweetalert2";
+import { AuthGuard } from "../../../guards/auth.guard";
+
+@Component({
+  selector: 'app-comment-list',
+  templateUrl: './comment-list.component.html',
+  styleUrls: ['./comment-list.component.css']
+})
+export class CommentListComponent implements OnInit {
+
+  @Input() comments:Comment[];
+  loggedIn: boolean;
+
+  constructor( private guard: AuthGuard) { }
+
+  ngOnInit(): void {
+  }
+
+  deleteComment(id: string): void {
+    this.loggedIn = this.guard.authenticated;
+    if (!this.loggedIn) {
+      void Swal.fire({
+        icon: "error",
+        title: "No tienes permisos para eliminar comentarios",
+        text: "Debes registrarte o iniciar sesión",
+      });
+
+      return;
+    }
+    //TODO DELETE comment
+  }
+}
